cae0cf8ca14f74ba93b3ab5c8820bec32114b4de
yichunlin <yichunlin@youkeshu.com>
2017-05-05 09:36:41 +0000

    图形修改

--- src/main/webapp/html/js/self.js
+++ src/main/webapp/html/js/self.js
@@ -15,7 +15,7 @@ $(function() {
 });
 
 /////页面日期插件  定义id为 start   end
-
+/*
 window.L.open('wDate', {
     'obj' : document.getElementById('start'),    //需绑定的对象
     'eventType' : 'focus',    //绑定的触发事件,默认click
@@ -26,3 +26,19 @@ window.L.open('wDate', {
     'eventType' : 'focus',    //绑定的触发事件,默认click
     'params' : {'readOnly' : true}    //传递WdatePicker的参数
 });
+*/
+
+    jeDate({
+      dateCell:"#start_date",
+      format:"YYYY-MM-DD",
+      //isinitVal:true,
+      //isTime:true, //isClear:false,
+      minDate:"2014-09-19 00:00:00"
+    });
+    jeDate({
+      dateCell:"#end_date",
+      format:"YYYY-MM-DD",
+      //isinitVal:true,
+      //isTime:true, //isClear:false,
+      minDate:"2014-09-19 00:00:00"
+    });
