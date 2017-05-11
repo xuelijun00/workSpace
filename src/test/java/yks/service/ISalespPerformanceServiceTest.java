ae72806c03e8b85ef3cd4eb45f6108890bde961c
xuelijun <xuelijun@youkeshu.com>
2017-05-11 08:02:46 +0000

    销售业绩整体报表

---
+++ src/test/java/yks/service/ISalespPerformanceServiceTest.java
@@ -0,0 +1,42 @@
+package yks.service;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import com.yks.bi.dto.report.SalesPerformance;
+import com.yks.bi.dto.report.TargetCompletionRateVo;
+import com.yks.bi.service.report.ISalespPerformanceService;
+import com.yks.bi.service.report.ITargetCompletionRateService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration(value = "src/main/webapp")
+@ContextConfiguration(value = "classpath*:config/spring-mvc.xml")
+public class ISalespPerformanceServiceTest {
+
+	@Autowired
+	ISalespPerformanceService salespPerformanceService;
+	
+	@Test
+	public void selectAll() {
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        Calendar c = Calendar.getInstance();
+        //过去15天
+        c.setTime(new Date());
+        c.add(Calendar.DATE, - 14);
+        Date d = c.getTime();
+        String day = format.format(d);
+        List<SalesPerformance> list = salespPerformanceService.selectAll(d);
+        Assert.assertEquals(15, list.size());
+	}
+
+}
